7a79fbf473c31c560f023f7a4638d0f7d53b5642
Michael F. Herbst <info@michael-herbst.com>
2021-12-28 21:04:14 +0100

    Integrate example with AtomIO

--- examples/atomsbase.jl
+++ examples/atomsbase.jl
@@ -1,26 +1,9 @@
 using DFTK
 using AtomsBase
+using AtomIO
 using Unitful
 using UnitfulAtomic
 using PyCall
-
-# Super clunky ASE parser that should go to a separate package
-function load_system(file::AbstractString)
-    ase = pyimport_e("ase")
-    ispynull(ase) && error("Install ASE to load data from exteral files")
-    ase_atoms = pyimport("ase.io").read(file)
-
-    T = Float64
-    cell_julia = convert(Array, ase_atoms.cell)  # Array of arrays
-    box = [[T(cell_julia[j][i]) * u"Å" for i = 1:3] for j = 1:3]
-
-    atoms = map(ase_atoms) do at
-        atnum = convert(Int, at.number)
-        Atom(atnum, at.position * u"Å"; magnetic_moment=at.magmom)
-    end
-    bcs = [p ? Periodic() : DirichletZero() for p in ase_atoms.pbc]
-    atomic_system(atoms, box, bcs)
-end
 
 
 #
@@ -40,9 +23,9 @@ kin_1  = Model(system; terms=[Kinetic()])  # low-level interface
 lda_1  = model_LDA(system)                 # high-level interface
 
 #
-# AtomsBase from file
+# Use AtomIO package to read structure from file
 #
-system = load_system("/tmp/out.json")
+system = load_system(joinpath(@__DIR__, "out.json"))
 system = attach_psp(system; family="hgh", functional="lda")
 kin_2  = Model(system; terms=[Kinetic()])  # low-level interface
 lda_2  = model_LDA(system)                 # high-level interface
@@ -67,6 +50,6 @@ lattice = a / 2 * [[0 1 1.];
                    [1 0 1.];
                    [1 1 0.]]
 Si    = ElementPsp(:Si, psp=load_psp("hgh/lda/si-q4.hgh"))
-atoms = [Si => ones(3)/8, Si => -ones(3)/8]
+atoms = [Si => [ones(3)/8, -ones(3)/8]]
 kin_4 = Model(lattice; atoms, terms=[Kinetic()])
 lda_4 = model_LDA(lattice, atoms)
